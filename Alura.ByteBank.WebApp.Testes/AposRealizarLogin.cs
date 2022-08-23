728e2d00ad82a795fd0aa95079e5aec0060e83fb
Cleriston Novaes <novaes.cleriston@gmail.com>
2022-08-23 15:54:30 -0300

    Testando WebApp

---
+++ Alura.ByteBank.WebApp.Testes/AposRealizarLogin.cs
@@ -0,0 +1,85 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Alura.ByteBank.WebApp.Testes
+{
+    public class AposRealizarLogin
+    {
+        [Fact]
+        public void AposRealizarLoginVerificaSeExisteOpcaoAgenciaMenu()
+        {
+            //Arrange
+            IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            driver.Navigate().GoToUrl("https://localhost:44309/UsuarioApps/Login");
+
+            var login = driver.FindElement(By.Id("Email")); //Selecionar elementos do HTML
+            var senha = driver.FindElement(By.Id("Senha")); //Selecionar elementos do HTML
+            var btnLogar = driver.FindElement(By.Id("btn-logar")); //Selecionar elementos do HTML
+
+            login.SendKeys("andre@email.com");
+            senha.SendKeys("senha01");
+
+            //Act
+            btnLogar.Click();
+
+            //Assert
+            Assert.Contains("Agência", driver.PageSource);
+
+        }
+
+        [Fact]
+        public void TentaRealizarLoginSemPreencherCampos()
+        {
+            //Arrange
+            IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            driver.Navigate().GoToUrl("https://localhost:44309/UsuarioApps/Login");
+
+            var login = driver.FindElement(By.Id("Email")); //Selecionar elementos do HTML
+            var senha = driver.FindElement(By.Id("Senha")); //Selecionar elementos do HTML
+            var btnLogar = driver.FindElement(By.Id("btn-logar")); //Selecionar elementos do HTML
+
+            //login.SendKeys("andre@email.com");
+            //senha.SendKeys("senha01");
+
+            //Act
+            btnLogar.Click();
+
+            //Assert
+            Assert.Contains("The Email field is required.", driver.PageSource);
+            Assert.Contains("The Senha field is required.", driver.PageSource);
+
+
+        }
+
+        [Fact]
+        public void TentaRealizarLoginComSenhaInvalida()
+        {
+            //Arrange
+            IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            driver.Navigate().GoToUrl("https://localhost:44309/UsuarioApps/Login");
+
+            var login = driver.FindElement(By.Id("Email")); //Selecionar elementos do HTML
+            var senha = driver.FindElement(By.Id("Senha")); //Selecionar elementos do HTML
+            var btnLogar = driver.FindElement(By.Id("btn-logar")); //Selecionar elementos do HTML
+
+            login.SendKeys("andre@email.com");
+            senha.SendKeys("senha010"); //Senha Inválida
+
+            //Act
+            btnLogar.Click();
+
+            //Assert
+            Assert.Contains("Login", driver.PageSource);
+
+
+        }
+    }
+}
---
+++ Alura.ByteBank.WebApp.Testes/NavegandoNaPaginaHome.cs
@@ -0,0 +1,111 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Alura.ByteBank.WebApp.Testes
+{
+    public class NavegandoNaPaginaHome
+    {
+        [Fact]
+        public void CarregaPaginaHomeEVerificaTituloDaPagina()
+        {
+            //Arrange
+            IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+
+            //Act
+            driver.Navigate().GoToUrl("https://localhost:44309");
+
+            //Assert
+            Assert.Contains("WebApp", driver.Title);
+
+        }
+
+        [Fact]
+        public void CarregadaPaginaHomeVerificaExistenciaLinkLoginEHome()
+        {
+            //Arrange
+            IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+
+            //Act
+            driver.Navigate().GoToUrl("https://localhost:44309");
+
+            //Assert
+            Assert.Contains("Login", driver.PageSource);
+            Assert.Contains("Home", driver.PageSource);
+        }
+
+        [Fact]
+        public void LogandoSistema()
+        {
+            //Arrange
+            IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            
+            //Act
+            driver.Navigate().GoToUrl("https://localhost:44309/");
+            driver.Manage().Window.Size = new System.Drawing.Size(1360, 728);
+            driver.FindElement(By.LinkText("Login")).Click();
+            driver.FindElement(By.Id("Email")).Click();
+            driver.FindElement(By.Id("Email")).SendKeys("andre@email.com");
+            driver.FindElement(By.Id("Senha")).Click();
+            driver.FindElement(By.Id("Senha")).SendKeys("senha01");
+            driver.FindElement(By.Id("btn-logar")).Click();
+            driver.FindElement(By.CssSelector(".btn")).Click();
+            driver.Close();
+        }
+
+        [Fact]
+        public void ValidaLinkDeLoginNaHome()
+        {
+            //Arrange
+            IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            driver.Navigate().GoToUrl("https://localhost:44309/");
+
+            var linkLogin = driver.FindElement(By.LinkText("Login"));
+
+            //Act
+            linkLogin.Click();
+
+            //Assert
+            Assert.Contains("img", driver.PageSource);
+
+        }
+
+        [Fact]
+        public void TentaAcessarPaginaSemEstarLogado()
+        {
+            //Arrange
+            IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+
+
+            //Act
+            driver.Navigate().GoToUrl("https://localhost:44309/Agencia/Index");
+
+
+            //Assert
+            Assert.Contains("401", driver.PageSource);
+        }
+
+        [Fact]
+        public void AcessaPaginaSemEstarLogadoVerificaURL()
+        {
+            //Arrange
+            IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+
+
+            //Act
+            driver.Navigate().GoToUrl("https://localhost:44309/Agencia/Index");
+
+
+            //Assert
+            Assert.Contains("https://localhost:44309/Agencia/Index", driver.Url);
+            Assert.Contains("401", driver.PageSource);
+        }
+    }
+}
